3fbfa83adba6dddf2edc6f83afca4ef054bdf36d
pavelperc <pavelperc@mail.ru>
2018-12-02 04:16:16 +0300

    Simple vue js client with a table.

--- src/main/kotlin/com/pavelperc/tutors/controller/PersonController.kt
+++ src/main/kotlin/com/pavelperc/tutors/controller/PersonController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/persons")
 class PersonController {
     
     @GetMapping
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,40 @@
+// how to create, sort, filter tables with vue js:
+// http://www.developerdrive.com/2017/07/creating-a-data-table-in-vue-js/
+
+var personApi = Vue.resource('/api/persons');
+
+
+Vue.component('persons-list', {
+    props: ['persons'],
+    template:
+        '<table id="firstTable" class="table table-striped" style="font-size: 16px;"">\n' +
+        '    <thead>\n' +
+        '    <tr>\n' +
+        '        <th>id</th>\n' +
+        '        <th>login</th>\n' +
+        '    </tr>\n' +
+        '    </thead>\n' +
+        '    <tbody>\n' +
+        '    <tr v-for="row in persons">\n' +
+        '        <td>{{row.id}}</td>\n' +
+        '        <td>{{row.login}}</td>\n' +
+        '    </tr>\n' +
+        '    </tbody>\n' +
+        '</table>',
+    created: function () {
+        personApi.get().then(result =>
+            result.json().then(data =>
+                data.forEach(person => this.persons.push(person))
+            )
+        )
+    }
+});
+
+
+var app = new Vue({
+    el: '#app',
+    template: '<persons-list :persons="persons" />',
+    data: {
+        persons: []
+    }
+});
