4d5a0cde9948418d9b1110d119f8ee3f60ef0eb0
pavelperc <pavelperc@mail.ru>
2018-12-02 03:14:25 +0300

    My first working controller!!!

---
+++ src/main/kotlin/com/pavelperc/tutors/controller/PersonController.kt
@@ -0,0 +1,20 @@
+package com.pavelperc.tutors.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("api")
+class PersonController {
+    
+    @GetMapping
+    fun allPersons(): List<Map<String, String>> {
+        return listOf(
+                mapOf(Pair("id", "1"), Pair("login", "pavel")),
+                mapOf(Pair("id", "2"), Pair("login", "ivan")),
+                mapOf(Pair("id", "3"), Pair("login", "alex"))
+        )
+    }
+}
